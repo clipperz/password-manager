9474b9ba38229f95aa46fe5a51a8ea7487d46293
Giulio Cesare Solaroli <giulio.cesare@clipperz.com>
2011-10-04 22:52:52 +0100

    Fixed the copyright header on the Russian translation file

--- frontend/beta/js/Clipperz/PM/Strings/Strings_ru-RU.js
+++ frontend/beta/js/Clipperz/PM/Strings/Strings_ru-RU.js
@@ -1,27 +1,28 @@
 /*
 
-Copyright 2008 Clipperz Srl
+Copyright 2008-2011 Clipperz Srl
 
-This file is part of Clipperz Community Edition.
-Clipperz Community Edition is a web-based password manager and a
-digital vault for confidential data.
+This file is part of Clipperz's Javascript Crypto Library.
+Javascript Crypto Library provides web developers with an extensive
+and efficient set of cryptographic functions. The library aims to
+obtain maximum execution speed while preserving modularity and
+reusability.
 For further information about its features and functionalities please
 refer to http://www.clipperz.com
 
-* Clipperz Community Edition is free software: you can redistribute
+* Javascript Crypto Library is free software: you can redistribute
   it and/or modify it under the terms of the GNU Affero General Public
   License as published by the Free Software Foundation, either version
   3 of the License, or (at your option) any later version.
 
-* Clipperz Community Edition is distributed in the hope that it will
+* Javascript Crypto Library is distributed in the hope that it will
   be useful, but WITHOUT ANY WARRANTY; without even the implied
   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Affero General Public License for more details.
 
 * You should have received a copy of the GNU Affero General Public
-  License along with Clipperz Community Edition.  If not, see
+  License along with Javascript Crypto Library.  If not, see
   <http://www.gnu.org/licenses/>.
-
 
 */
 
