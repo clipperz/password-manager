b86ba23979b096075746790b9351bb123b9d76f4
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-09-16 18:37:36 +0200

    Add function to extract exceptT from stateT
    Co-authored-by: Fabio Solaroli <fsolaroli@users.noreply.github.com>

--- src/main/purescript/Functions/State.purs
+++ src/main/purescript/Functions/State.purs
@@ -1,8 +1,25 @@
 module Functions.State where
 
-import Control.Monad.State (StateT(..))
+import Control.Applicative (pure)
+import Control.Bind (bind, discard)
+import Control.Monad (class Monad)
+import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
+import Control.Monad.State (StateT(..), runStateT, modify_)
+import Data.Either (Either(..))
+import Data.Function (($))
 import Data.Functor (class Functor, (<$>))
 import Data.Tuple (Tuple(..))
 
 makeStateT :: forall m a s. Functor m => m a -> StateT s m a
 makeStateT value = StateT (\s -> ((\r -> Tuple r s) <$> value))
+
+extractExceptT :: forall a s e m. Monad m => StateT s (ExceptT e m) a -> s -> StateT s m (Either e a)
+extractExceptT monad state = do
+  result <- makeStateT $ runExceptT $ runStateT monad state
+  case result of
+    Left err -> do
+      modify_ (\_ -> state)
+      makeStateT $ pure $ Left err
+    Right (Tuple value newState) -> do
+      modify_ (\_ -> newState)
+      makeStateT $ pure $ Right value
