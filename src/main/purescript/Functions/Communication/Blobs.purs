e801c89328ad1fc255e6b5a8977683ad448a94cc
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-09-16 18:38:02 +0200

    Use StateT in HomePageManager and blobs functions
    Co-authored-by: Fabio Solaroli <fsolaroli@users.noreply.github.com>

--- src/main/purescript/Functions/Communication/Blobs.purs
+++ src/main/purescript/Functions/Communication/Blobs.purs
@@ -4,7 +4,8 @@ import Affjax.Web as AXW
 import Affjax.RequestBody (RequestBody, json)
 import Affjax.ResponseFormat as RF
 import Control.Bind (bind)
-import Control.Monad.Except.Trans (ExceptT(..), runExceptT, except, withExceptT)
+import Control.Monad.Except.Trans (ExceptT(..), except, withExceptT)
+import Control.Monad.State (StateT)
 import Crypto.Subtle.Key.Types (CryptoKey)
 import Data.Argonaut.Encode.Class (encodeJson)
 import Data.Argonaut.Decode.Class (class DecodeJson)
@@ -17,35 +18,30 @@ import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
 import Data.Show (show)
 import Data.String.Common (joinWith)
+import DataModel.AppState (AppState)
 import DataModel.Communication.ProtocolError (ProtocolError(..))
 import Effect.Aff (Aff)
 import EncodeDecode (decryptJson)
-import Functions.Communication.BackendCommunication (isStatusCodeOk, doGenericRequest')
+import Functions.Communication.BackendCommunication (isStatusCodeOk, manageGenericRequest)
+import Functions.State (makeStateT)
 
 -- ----------------------------------------------------------------------------
 
-getBlob :: HexString ->  Aff (Either ProtocolError (AXW.Response ArrayBuffer))
--- getBlob :: HexString -> StateT AppState Aff (Either ProtocolError (AXW.Response ArrayBuffer))
+getBlob :: HexString -> StateT AppState (ExceptT ProtocolError Aff) (AXW.Response ArrayBuffer)
 getBlob hash = do
-  let url = joinWith "/" ["http://localhost:8090", "blobs", show $ hash]
-  doGenericRequest' url GET [] Nothing RF.arrayBuffer
+  let url = joinWith "/" ["blobs", show $ hash]
+  manageGenericRequest url GET Nothing RF.arrayBuffer
 
-getDecryptedBlob :: forall a. DecodeJson a => HexString -> CryptoKey -> Aff (Either ProtocolError a)
-getDecryptedBlob reference key = runExceptT $ do
-  response <- ExceptT $ getBlob reference
-  if isStatusCodeOk response.status
+getDecryptedBlob :: forall a. DecodeJson a => HexString -> CryptoKey -> StateT AppState (ExceptT ProtocolError Aff) a 
+getDecryptedBlob reference key = do
+  response <- getBlob reference
+  makeStateT $ if isStatusCodeOk response.status
     then withExceptT (\e -> CryptoError $ show e) (ExceptT $ decryptJson key response.body)
     else except $ Left (ResponseError (unwrap response.status))
--- getDecryptedBlob :: forall a. DecodeJson a => HexString -> CryptoKey -> StateT AppState Aff (Either ProtocolError a)
--- getDecryptedBlob reference key = mapStateT $ runExceptT $ do
---   response <- mapStateT ExceptT $ getBlob reference
---   makeStateT $ if isStatusCodeOk response.status
---                then withExceptT (\e -> CryptoError $ show e) (ExceptT $ decryptJson key response.body)
---                else except $ Left (ResponseError (unwrap response.status))
 
-postBlob :: ArrayBuffer -> Aff (Either ProtocolError (AXW.Response String))
--- postBlob :: ArrayBuffer -> StateT AppState Aff (Either ProtocolError (AXW.Response String))
+postBlob :: ArrayBuffer -> StateT AppState (ExceptT ProtocolError Aff) (AXW.Response String)
 postBlob blob = do
-  let url = joinWith "/" ["http://localhost:8090", "blobs"]
+  let url = joinWith "/" ["blobs"]
   let body = json $ encodeJson (fromArrayBuffer blob) :: RequestBody
-  doGenericRequest' url POST [] (Just body) RF.string
+  manageGenericRequest url POST (Just body) RF.string
+
--- src/main/purescript/WidgetManagers/HomePageManager.purs
+++ src/main/purescript/WidgetManagers/HomePageManager.purs
@@ -5,76 +5,84 @@ import Concur.Core.FRP (loopW, dyn)
 import Concur.React (HTML)
 import Control.Applicative (pure)
 import Control.Bind (discard, bind)
-import Control.Monad.Except.Trans (ExceptT(..), runExceptT, withExceptT)
-import Control.Monad.State (StateT, get)
+import Control.Monad.Except.Trans (ExceptT(..), withExceptT)
+import Control.Monad.State (StateT, get, runStateT, modify_, mapStateT)
 import Control.Semigroupoid ((>>>))
 import Crypto.Subtle.Constants.AES (aesCTR)
 import Crypto.Subtle.Key.Import as KI
 import Crypto.Subtle.Key.Types (encrypt, decrypt, raw, unwrapKey, CryptoKey)
+import Data.ArrayBuffer.Types (ArrayBuffer)
 import Data.Either (Either(..))
-import Data.Function (($))
+import Data.Function (($), flip)
 import Data.Functor ((<$>))
 import Data.HexString (HexString, toArrayBuffer, fromArrayBuffer, splitHexInHalf)
-import Data.List.Types (List(..))
 import Data.Maybe (Maybe(..))
 import Data.Show (show)
+import Data.Tuple (Tuple(..))
 import Data.Unit (Unit, unit)
 import DataModel.AppState (AppState)
-import DataModel.Card (Card(..), card0)
-import DataModel.Index (CardReference(..), Index(..), IndexReference)
+import DataModel.Card (Card)
+import DataModel.Communication.ProtocolError (ProtocolError(..))
+import DataModel.Index (CardReference(..), Index, IndexReference)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (liftAff)
-import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
 import Effect.Exception as EX
 import EncodeDecode (decryptArrayBuffer)
 import Functions.Communication.Blobs (getDecryptedBlob)
-import Functions.State (makeStateT)
+import Functions.State (makeStateT, extractExceptT)
 import Widgets.HomePage (HomePageAction(..), CardsViewAction(..), homePage)
 
-getCard :: CardReference -> Aff Card
+getCard :: CardReference -> StateT AppState Aff (Either ProtocolError Card)
 getCard (CardReference_v1 { reference, key }) = do
-  cryptoKey <- KI.importKey raw (toArrayBuffer key) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
-  result <- getDecryptedBlob reference cryptoKey
-  case result of
-    Left err -> do
-      _ <- log $ show err
-      _ <- log $ show "False Card"
-      pure $ Card_v1 { content: card0, timestamp: 1661377622} -- TODO
-    Right card -> pure card
+  currentState <- get
+  cryptoKey <- makeStateT $ KI.importKey raw (toArrayBuffer key) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
+  extractExceptT (getDecryptedBlob reference cryptoKey) currentState 
 
-getIndex :: HexString -> HexString -> Aff Index
-getIndex p encryptedRef = do
-  result <- runExceptT $ do
-    masterPassword :: CryptoKey <- ExceptT $ Right <$> KI.importKey raw (toArrayBuffer p) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
-    { before: masterKey, after: indexReference } <- ExceptT $ (\either -> (fromArrayBuffer >>> splitHexInHalf) <$> either) <$> (decryptArrayBuffer masterPassword (toArrayBuffer encryptedRef))
-    cryptoKey      :: CryptoKey <- ExceptT $ Right <$> KI.importKey raw (toArrayBuffer masterKey) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
-    withExceptT (\err -> EX.error $ show err) (ExceptT $ getDecryptedBlob indexReference cryptoKey)
-  case result of
-    -- Left  _   -> getIndex p encryptedRef
-    Left  err -> do
-      _ <- log $ show err
-      _ <- log $ show "False Index"
-      pure $ Index_v1 Nil -- TODO
-    Right res -> pure res
+getIndex :: HexString -> HexString -> StateT AppState Aff (Either ProtocolError Index)
+getIndex p encryptedRef = do -- StateT Aff
+  currentState <- get
+  (flip extractExceptT) currentState $ do
+    masterPassword :: CryptoKey <- makeStateT $ ExceptT $ Right <$> KI.importKey raw (toArrayBuffer p) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
+    { before: masterKey, after: indexReference } <- makeStateT $ mapDecodeError $ ExceptT $ splitInHalf <$> (decryptEncryptedRef masterPassword)
+    cryptoKey      :: CryptoKey <- makeStateT $ ExceptT $ Right <$> KI.importKey raw (toArrayBuffer masterKey) (KI.aes aesCTR) false [encrypt, decrypt, unwrapKey]
+    getDecryptedBlob indexReference cryptoKey
+  
+  where 
+    decryptEncryptedRef :: CryptoKey -> Aff (Either EX.Error ArrayBuffer)
+    decryptEncryptedRef password = decryptArrayBuffer password (toArrayBuffer encryptedRef)
+    splitInHalf :: Either EX.Error ArrayBuffer -> Either EX.Error { before :: HexString, after :: HexString }
+    splitInHalf either = (fromArrayBuffer >>> splitHexInHalf) <$> either
+    mapDecodeError :: ExceptT EX.Error Aff { before :: HexString, after :: HexString } -> ExceptT ProtocolError Aff { before :: HexString, after :: HexString }
+    mapDecodeError = withExceptT (\e -> DecodeError $ EX.message e)
 
--- homePageManager :: IndexReference -> StateT AppState (Widget HTML) Unit
 homePageManager :: IndexReference -> StateT AppState (Widget HTML) Unit
 homePageManager indexReference = do
   currentState <- get
   case currentState of
-    { p: Just p, sessionKey: _, c: _, proxy: _, toll: _ } -> makeStateT $ do
-      index <- liftAff $ getIndex p indexReference
-      dyn $ loopW (CardsViewAction (ShowCard Nothing)) (\cva ->
-        case cva of
-          CardsViewAction (ShowCard Nothing) -> homePage index Nothing
-          CardsViewAction (ShowCard (Just ref)) -> do
-            card <- liftAff $ getCard ref
-            homePage index (Just card)
-          CardsViewAction (ActOnCard _ a) -> do
-            liftEffect $ log $ show a
-            homePage index Nothing
-      )
+    { p: Just p, sessionKey: _, c: _, proxy: _, toll: _ } -> do
+      eitherIndex <- mapStateT (\e -> liftAff e) $ getIndex p indexReference
+      case eitherIndex of
+        Right index -> do
+          Tuple result newState <- makeStateT $ dyn $ loopW (Tuple (CardsViewAction (ShowCard Nothing)) currentState) (\(Tuple cva s) ->
+            case cva of
+              CardsViewAction (ShowCard Nothing) -> (\r -> Tuple r s) <$> homePage index Nothing
+              CardsViewAction (ShowCard (Just ref)) -> do
+                Tuple either newState <- liftAff $ runStateT (getCard ref) s
+                case either of
+                  Left err -> do
+                    _ <- log $ show err
+                    (\r -> Tuple r s) <$> homePage index Nothing
+                  Right card -> (\r -> Tuple r newState) <$> homePage index (Just card)
+              CardsViewAction (ActOnCard _ a) -> do
+                _ <- log $ show a
+                (\r -> Tuple r s) <$> homePage index Nothing
+          )
+          modify_ (\_ -> newState)
+          pure result
+        Left err -> do
+          _ <- log $ show err
+          pure unit
     _ -> do
       log $ show currentState
       makeStateT $ pure $ unit -- TODO: manage error
