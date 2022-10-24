15bf747a66f2c49d0f2a38ba3e763b96e0dee4cd
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-10-24 16:05:43 +0200

    Implement user archive tests

--- src/test/scala/is/clipperz/backend/services/UserArchive.scala
+++ src/test/scala/is/clipperz/backend/services/UserArchive.scala
@@ -27,6 +27,8 @@ import is.clipperz.backend.data.HexString
 import is.clipperz.backend.exceptions.BadRequestException
 import zio.test.TestEnvironment
 import zio.ZLayer
+import is.clipperz.backend.exceptions.ResourceConflictException
+import zio.test.TestConsole
 
 object UserArchiveSpec extends ZIOSpecDefault:
   val userBasePath = FileSystems.getDefault().nn.getPath("target", "tests", "archive", "users").nn
@@ -34,8 +36,8 @@ object UserArchiveSpec extends ZIOSpecDefault:
   val environment = UserArchive.fs(userBasePath, 2)
 
   val c = HexString("abcdef0192837465")
-  val testUser = UserCard(c, HexString("s"), HexString("v"), "srpVersion_test", "masterKeyEncodingVersion_test", HexString("masterKeyContent_test"))
-  val testUser2 = UserCard(c, HexString("s2"), HexString("v"), "srpVersion_test", "masterKeyEncodingVersion_test", HexString("masterKeyContent_test"))
+  val testUser = UserCard(c, HexString("adc"), HexString("adcf"), "srpVersion_test", "masterKeyEncodingVersion_test", HexString("masterKeyContent_test"))
+  val testUser2 = UserCard(c, HexString("adc2"), HexString("adcf2"), "srpVersion_test", "masterKeyEncodingVersion_test", HexString("masterKeyContent_test"))
 
  
   def spec = suite("UserArchive")(
@@ -54,38 +56,39 @@ object UserArchiveSpec extends ZIOSpecDefault:
         user <- archive.getUser(c)
       } yield assertTrue(user == Some(testUser))
     } + 
-    test ("saveBlob with failing stream - success") {
+    test ("saveBlob with no overwrite - fail") {
       for {
-       archive <- ZIO.service[UserArchive]
-       fiber <- archive.saveBlob(failingKey, failingContent).fork
+        archive <- ZIO.service[UserArchive]
+        fiber <- archive.saveUser(testUser2, false).fork
         _ <- TestClock.adjust(Duration.fromMillis(10))
-        res <- assertZIO(fiber.await)(fails(isSubtype[EmptyContentException](anything)))
+        res <- assertZIO(fiber.await)(fails(isSubtype[ResourceConflictException](anything)))
       } yield assertTrue(res.isSuccess)
     } + 
-    test ("saveBlob with wrong hash - success") {
+    test ("saveBlob with overwrite - success") {
       for {
-       archive <- ZIO.service[UserArchive]
-       fiber <- archive.saveBlob(failingKey, testContent).fork
+        archive <- ZIO.service[UserArchive]
+        fiber <- archive.saveUser(testUser2, true).fork
         _ <- TestClock.adjust(Duration.fromMillis(10))
-        res <- assertZIO(fiber.await)(fails(isSubtype[BadRequestException](anything)))
-      } yield assertTrue(res.isSuccess)
+        _ <- fiber.join
+        s2 <- archive.getUser(c).map(_.map(_.s))
+      } yield assertTrue(s2 == Some(testUser2.s))
     } +
     test("getUser - success") {
       for {
         archive <- ZIO.service[UserArchive]
         user <- archive.getUser(c)
-      } yield assertTrue(user == Some(testUser))
+      } yield assertTrue(user == Some(testUser2))
     } + 
     test("deleteBlob - success") {
       for {
         archive <- ZIO.service[UserArchive]
-        res <- archive.deleteBlob(testContent)
+        res <- archive.deleteUser(c)
       } yield assertTrue(res)
     } + 
     test("deleteBlob - fail") {
       for {
         archive <- ZIO.service[UserArchive]
-        res <- archive.deleteBlob(testContent)
+        res <- archive.deleteUser(c)
       } yield assertTrue(!res)
     }
   ).provideSomeLayerShared(environment) @@ 
