8a9b0671fa3e8d80cc97e0649644ba6c28870428
Fabio Solaroli <fabio@solaroli.it>
2022-10-28 03:05:53 +0200

    Create convertion from Card_V1 to Card

--- src/main/purescript/DataModel/CardVersions/CardV1.purs
+++ src/main/purescript/DataModel/CardVersions/CardV1.purs
@@ -1,5 +1,8 @@
 module DataModel.CardVersions.CardV1 where
 
+import Data.Functor ((<$>))
+import DataModel.Card (Card(..), CardValues(..), CardField(..))
+
 type Card_V1 = 
   { content :: CardValues_V1
   , archived :: Boolean
@@ -18,3 +21,6 @@ type CardField_V1 =
   , value  :: String
   , locked :: Boolean
   }
+
+cardFromV1 :: Card_V1 -> Card
+cardFromV1 card = Card card { content = CardValues card.content { fields = CardField <$> card.content.fields } }
