e8b0786184ef621bf0a49368ebb124919cf2cb7c
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-11-11 15:48:59 +0100

    Improve user delete page

--- src/main/purescript/OperationalWidgets/DeleteUserWidget.purs
+++ src/main/purescript/OperationalWidgets/DeleteUserWidget.purs
@@ -1,30 +1,40 @@
 module OperationalWidgets.DeleteUserWidget where
 
 import Concur.Core (Widget)
+import Concur.Core.FRP (demand, loopS, loopW, fireOnce)
 import Concur.React (HTML)
-import Concur.React.DOM (text, div, div')
+import Concur.React.DOM (text, div, h1, div')
 import Control.Alternative ((<|>))
 import Control.Applicative (pure)
-import Control.Bind (bind)
-import Control.Monad.Except.Trans (runExceptT)
-import Data.Either (either)
+import Control.Bind (bind, discard)
+import Control.Monad.Except.Trans (runExceptT, except, ExceptT(..))
+import Data.Either (either, Either(..))
+import Data.Eq ((==))
 import Data.Function (($))
-import Data.Functor ((<$>))
+import Data.Functor ((<$>), (<$))
+import Data.HeytingAlgebra ((||), (&&), not)
+import Data.Maybe (Maybe(..))
 import Data.Show (show)
 import Data.Unit (Unit, unit)
 import DataModel.AppState (AppError)
 import DataModel.Index (Index)
 import DataModel.WidgetState (WidgetState(..))
+import Effect (Effect)
 import Effect.Aff.Class (liftAff)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
+import Functions.JSState (getAppState)
 import Functions.User (deleteUser)
-import Views.SimpleWebComponents (loadingDiv, simpleButton, confirmationWidget)
+import Record (merge)
+import Views.LoginFormView (emptyForm)
+import Views.SimpleWebComponents (loadingDiv, simpleButton, confirmationWidget, simpleUserSignal, simpleCheckboxSignal, simplePasswordInputWidget)
 
 data DeleteUserWidgetAction = PleaseDelete | DoNothing | FailedDelete AppError | Done
 
 deleteUserWidget :: Index -> WidgetState -> Widget HTML Unit
 deleteUserWidget index state = do
   res <- case state of
-    Default -> div' [simpleButton "Delete account" false PleaseDelete]
+    Default -> deleteForm emptyForm
     Loading -> do
       let deleteUserOp = liftAff $ (either FailedDelete (\_ -> Done) <$> (runExceptT $ deleteUser index))
       div' [loadingDiv, simpleButton "Delete account" true DoNothing] <|> deleteUserOp
@@ -36,3 +46,31 @@ deleteUserWidget index state = do
     DoNothing -> deleteUserWidget index Default
     FailedDelete err -> deleteUserWidget index (Error (show err))
     Done -> pure unit
+
+  where
+    deleteForm creds@{ username, password } = div [] [
+      h1 [] [text "Delete account"]
+    , demand $ do
+        formValues <- loopS (merge creds {notRecoverable: false}) $ \{username, password, notRecoverable} -> do
+          username' :: String <- simpleUserSignal username
+          password' :: String <- loopW password (simplePasswordInputWidget "password" (text "Password"))
+          checkbox' :: Boolean <- simpleCheckboxSignal "not_recoverable" (text "All my data will be permanently deleted. I understand that this action cannot be undone or canceled.") true notRecoverable
+          pure { username: username', password: password', notRecoverable: checkbox' }
+        fireOnce (submitWidget formValues)
+    ] 
+
+    submitWidget :: { username :: String, password :: String, notRecoverable :: Boolean } -> Widget HTML DeleteUserWidgetAction
+    submitWidget f@{ username, password, notRecoverable } = do
+      check <- liftEffect $ if username == "" || password == "" then pure (Right false) else checkC f
+      case check of
+        Left err -> do
+          log $ show err
+          simpleButton "Delete account" true PleaseDelete
+        Right b -> do
+          let enable = b && notRecoverable
+          simpleButton "Delete account" (not enable) PleaseDelete
+    
+    checkC :: { username :: String, password :: String, notRecoverable :: Boolean } -> Effect (Either AppError Boolean)
+    checkC { username, password } = runExceptT $ do
+      { username: u, password: p } <- ExceptT $ liftEffect $ getAppState
+      except $ Right $ u == (Just username) && p == (Just password)
