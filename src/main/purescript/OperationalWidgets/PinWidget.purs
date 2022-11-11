c9282f82569b10311bd6fc3292d8e010ad6f576d
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-11-11 14:29:49 +0100

    Improve pin page

--- src/main/purescript/OperationalWidgets/PinWidget.purs
+++ src/main/purescript/OperationalWidgets/PinWidget.purs
@@ -4,7 +4,9 @@ import Bytes (asArrayBuffer)
 import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.Core.FRP (demand, fireOnce, loopW)
-import Concur.React.DOM (div, fieldset, text)
+import Concur.React.DOM (h1, p, div, fieldset, text)
+import Concur.React.Props as Props
+import Control.Alternative ((<|>))
 import Control.Applicative (pure)
 import Control.Bind (bind, discard, (>>=))
 import Control.Monad.Except.Trans (ExceptT(..), runExceptT, except)
@@ -12,16 +14,18 @@ import Control.Semigroupoid ((<<<))
 import Data.Either (note, Either(..))
 import Data.EuclideanRing ((/))
 import Data.Function (($))
-import Data.Functor ((<$>))
+import Data.Functor ((<$>), void)
 import Data.HexString (fromArrayBuffer, hex, toArrayBuffer)
 import Data.HeytingAlgebra (not)
 import Data.Int (fromString)
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..), isJust)
 import Data.Ring ((-))
+import Data.Semigroup ((<>))
 import Data.Semiring ((*))
 import Data.Show (show)
 import Data.String.CodeUnits (length)
+import Data.Unfoldable (fromMaybe)
 import Data.Unit (Unit)
 import DataModel.AppState (AppError(..), InvalidStateError(..))
 import DataModel.WidgetState (WidgetState(..))
@@ -46,12 +50,12 @@ setPinWidget ws = do
   maybeSavedUser <- liftEffect $ getItem (makeKey "user") storage
   let pinForm = form (isJust maybeSavedUser)
   pinAction <- case ws of
-    Default -> div [] [pinForm]
-    Loading -> div [] [pinForm]
-    Error e -> div [] [div [] [text e], pinForm]
+    Default -> pinPage Nothing  pinForm
+    Loading -> pinPage Nothing  pinForm
+    Error e -> pinPage (Just e) pinForm
   eitherRes <- case pinAction of
-    Reset -> runExceptT (deleteCredentials storage)
-    SetPin pin -> runExceptT (saveCredentials pin storage) 
+    Reset -> (Right <$> (void $ pinPage Nothing (form true))) <|> (runExceptT (deleteCredentials storage))
+    SetPin pin -> (Right <$> (void $ pinPage Nothing (form true))) <|> (runExceptT (saveCredentials pin storage)) 
   case eitherRes of
     Left err -> setPinWidget (Error (show err))
     _        -> setPinWidget Default
@@ -59,7 +63,8 @@ setPinWidget ws = do
   where 
     form :: Boolean -> Widget HTML PinWidgetAction
     form pinExists = fieldset [] [
-      do
+      text $ "PIN is " <> (if pinExists then "" else "not ") <> "set on this device"
+    , do
         signalResult <- demand $ do
           pin <- loopW "" (\value -> simpleInputWidget "pinField" (text "PIN") pinExists (if pinExists then "*****" else "PIN") value "number")
           result :: Maybe PinWidgetAction <- fireOnce (submitWidget pin pinExists)
@@ -98,4 +103,12 @@ setPinWidget ws = do
       removeItem (makeKey "passphrase") storage
       removeItem (makeKey "failures") storage
 
-
+pinPage :: forall a. Maybe String -> Widget HTML a -> Widget HTML a
+pinPage error internalForm = div [Props._id "pinPage"] [
+  h1 [] [text "Device PIN"]
+, div [Props.className "description"] $ [
+    p [] [text "You may create a 5-digit PIN to be used instead of your passphrase. Please note that the PIN is specific to the device you are now using."]
+  , p [] [text "Warning: enabling a PIN on your device may represent a security risk! Make sure to keep the device with you at all times!"]  
+  ] <> (fromMaybe $ (\t -> p [Props.className "error"] [text t]) <$> error)
+, div [Props.className "content"] [internalForm]
+]
