8ca3b1c9e5f55cfe1b3028cef6ffd10afeb72f69
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-11-11 18:25:26 +0100

    Improve export page

--- src/main/purescript/OperationalWidgets/ExportWidget.purs
+++ src/main/purescript/OperationalWidgets/ExportWidget.purs
@@ -5,7 +5,7 @@ module OperationalWidgets.ExportWidget
 
 import Concur.Core (Widget)
 import Concur.React (HTML)
-import Concur.React.DOM (div, a, text, button)
+import Concur.React.DOM (div, a, text, h1, h3, p, button)
 import Concur.React.Props as Props
 import Control.Alternative ((<|>))
 import Control.Bind (bind)
@@ -24,14 +24,25 @@ import Functions.JSState (getAppState)
 import Functions.Time (getCurrentDateTime, formatDateTimeToDate)
 
 exportWidget :: Index -> Widget HTML Unit
-exportWidget index = div [] [
-    div [] [ do
-      _ <- button [Props.onClick, Props.disabled false] [text "Prepare offline copy"]
-      downloadOfflineCopy index (void (button [Props.onClick, Props.disabled true] [text "Prepare offline copy"]))
+exportWidget index = div [Props._id "exportPage"] [
+    h1 [] [text "Export"]
+  , div [] [ 
+      h3 [] [text "Offline copy"]
+    , p [] [text "Download a read-only portable version of Clipperz. Very convenient when no Internet connection is available."]
+    , p [] [text "An offline copy is just a single HTML file that contains both the whole Clipperz web application and your encrypted data."]
+    , p [] [text "It is as secure as the hosted Clipperz service since they both share the same code and security architecture."]
+    , do
+        _ <- button [Props.onClick, Props.disabled false] [text "Prepare offline copy"]
+        downloadOfflineCopy index (void (button [Props.onClick, Props.disabled true] [text "Prepare offline copy"]))
     ]
-  , div [] [ do
-      _ <- button [Props.onClick, Props.disabled false] [text "Prepare unencrypted data"]
-      downloadUnencryptedCopy index (void (button [Props.onClick, Props.disabled true] [text "Prepare unencrypted data"]))
+  , div [] [ 
+      h3 [] [text "HTML + JSON"]
+    , p [] [text "Download a printer-friendly HTML file that lists the content of all your cards."]
+    , p [] [text "This same file also contains all your data in JSON format. Please note that file attachments are not included."]
+    , p [Props.className "important"] [text "Beware: all data are unencrypted! Therefore make sure to properly store and manage this file."]
+    , do
+        _ <- button [Props.onClick, Props.disabled false] [text "Prepare unencrypted data"]
+        downloadUnencryptedCopy index (void (button [Props.onClick, Props.disabled true] [text "Prepare unencrypted data"]))
     ]
   ]
 
