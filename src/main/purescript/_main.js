bd10bad59544864e78c1831c49fae004462d5837
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-11-14 11:54:30 +0100

    Manage keyboard shortcuts with mousetrap

--- src/main/purescript/_main.js
+++ src/main/purescript/_main.js
@@ -2,22 +2,18 @@ const { Even } = require("../../../target/output.purescript/Data.Int")
 const { delay } = require("../../../target/output.purescript/Effect.Aff")
 const Main = require ("../../../target/output.purescript/Main")
 
-function addKeyDownEventBlocker() {
+function addKeyDownEventBubblingBlocker() {
     const observer = new MutationObserver(mutations => {
         mutations.forEach(function(mutation) {
             for(var i = 0; i < mutation.addedNodes.length; i++)
                 mutation.addedNodes.forEach(node => {
                     try {
-                        console.log(node.id)
                         if (node.classList.contains("cardForm")) {
-                            console.log("Adding evl to " + node)
                             node.addEventListener("keydown", ev => {
-                                console.log("From listener " + ev)
                                 ev.stopImmediatePropagation();
                             })
                         } else {
                             document.getElementById("card").addEventListener("keydown", ev => {
-                                console.log("From listener' " + ev)
                                 ev.stopImmediatePropagation();
                             })
                         }
@@ -26,11 +22,36 @@ function addKeyDownEventBlocker() {
                 })
         })
     });
-    
     observer.observe(document.body, {
         childList: true,
         subtree: true,
     });
+}
+
+function addShortcutsManagement() {
+    Mousetrap.bind("/", function(ev) {
+        document.getElementById("generalFilter").focus();
+        ev.preventDefault();
+    })
+    Mousetrap.bind("*", function(ev) {
+        document.getElementById("generalFilter").value = "";
+        document.getElementById("generalFilter").focus();
+        document.getElementById("generalFilter").blur();
+    })
+    Mousetrap.bind("l o c  k", function(ev) {
+        document.getElementById("lockButton").dispatchEvent(new MouseEvent("click", {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+        }))
+    })
+    window.document.onkeydown = ev => {
+        if (ev.target.nodeName === "BODY") {
+            if (ev.type === "keydown") {
+                document.getElementById("cardsManager").dispatchEvent(new KeyboardEvent("keydown", ev))
+            }
+        }
+    }
 }
 
 function main () {
@@ -49,35 +70,8 @@ function main () {
         However, you will need to change the type to accept variables, by default it is an Effect.
         You will probably want to make it a function from String -> Effect ()
     */
-    addKeyDownEventBlocker();
-
-    const lastFourKeys = Array(4);
-    window.document.onkeydown = ev => {
-        console.log(ev)
-        if (ev.target.nodeName === "BODY") {
-            if (ev.key === "/") {
-                document.getElementById("generalFilter").focus()
-                ev.preventDefault()
-            } else if (ev.key === "*") {
-                document.getElementById("generalFilter").value = ""
-                document.getElementById("generalFilter").focus()
-                document.getElementById("generalFilter").blur()
-                ev.preventDefault()
-            } else if (ev.type === "keydown") {
-                if (lastFourKeys.push(ev.key) > 4) {
-                    lastFourKeys.shift()
-                }
-                if (lastFourKeys.reduce((a, b) => a + b) === "lock") {
-                    document.getElementById("lockButton").dispatchEvent(new MouseEvent("click", {
-                        bubbles: true,
-                        cancelable: true,
-                        view: window,
-                      }))
-                }
-                document.getElementById("cardsManager").dispatchEvent(new KeyboardEvent("keydown", ev))
-            }
-        }
-    }
+    addKeyDownEventBubblingBlocker();
+    addShortcutsManagement();
 
     Main.main();
 }
