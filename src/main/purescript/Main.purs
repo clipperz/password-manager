28d06e0525c227356443bceae30384e8c72411a8
Gio-R <37812474+Gio-R@users.noreply.github.com>
2022-11-28 12:27:03 +0100

    Add automatic dev login via url

--- src/main/purescript/Main.purs
+++ src/main/purescript/Main.purs
@@ -3,29 +3,56 @@ module Main
   )
   where
 
-import Data.Maybe (Maybe(..))
-import Data.Unit (Unit)
 import Concur.React.Run (runWidgetInDom)
+import Control.Bind (bind, discard, (>>=))
+import Data.Array (index, catMaybes)
+import Data.Function (($))
+import Data.Functor ((<$>))
+import Data.Map (fromFoldable, lookup)
+import Data.Maybe (Maybe(..))
+import Data.String (drop, split)
+import Data.String.Pattern (Pattern(..))
+import Data.Tuple (Tuple(..))
+import Data.Unit (Unit)
 import Effect (Effect)
 import OperationalWidgets.App (app, Page(..), SharedCardReference, doTestLogin)
 import Web.HTML (window)
-import Web.HTML.Location (hash)
+import Web.HTML.Location (hash, search)
 import Web.HTML.Window (location)
 
-import Control.Bind (bind, discard)
 import Effect.Class.Console (log)
+import Data.Semigroup ((<>))
+import Data.Show (show)
 
 main :: Effect Unit
 main = do
-  w <- window
-  l <- location w
+  l <- window >>= location
   h <- hash l
-  log h
+  s <- search l
   case h of
-    -- "#share" -> do
-      -- token <-
-      -- runWidgetInDom "app" (app (Share (Just token)))
     "#registration" -> runWidgetInDom "app" (app Signup)
-    "#login" -> do
-      runWidgetInDom "app" (doTestLogin "joe" "clipperz")
-    _ -> runWidgetInDom "app" (app (Loading (Just Login)))
+    str -> do
+      case split (Pattern "?") str of
+        [ "#login", query ] -> do
+          let parameters = fromFoldable $ parseQueryString query
+          log $ show parameters
+          case (lookup "username" parameters), (lookup "password" parameters) of
+            Just u, Just p -> runWidgetInDom "app" (doTestLogin u p)
+            _, _ -> defaultPage
+        [ "#share", query ] -> do
+          let parameters = fromFoldable $ parseQueryString query
+          case lookup "token" parameters of
+            Just token -> runWidgetInDom "app" (app (Share (Just token)))
+            _ -> defaultPage
+        r -> defaultPage
+
+defaultPage :: Effect Unit
+defaultPage = runWidgetInDom "app" (app (Loading (Just Login)))
+
+parseQueryString :: String -> Array (Tuple String String)
+parseQueryString query = 
+  let keyValues = split (Pattern "&") query
+      splitKeyValue = \s -> case split (Pattern "=") s of
+                              [key, value] -> Just $ Tuple key value
+                              _ -> Nothing
+  in catMaybes $ splitKeyValue <$> keyValues 
