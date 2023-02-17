c37d5c26bd284c7b048911cab8387922f670c0b9
Fabio Solaroli <fabio@solaroli.it>
2023-02-17 16:28:21 +0100

    Fixed styling for delete account confermation

--- src/main/purescript/OperationalWidgets/DeleteUserWidget.purs
+++ src/main/purescript/OperationalWidgets/DeleteUserWidget.purs
@@ -41,9 +41,12 @@ import Views.SimpleWebComponents (loadingDiv, loadingBar, simpleButton, confirma
 import Web.HTML (window)
 import Web.HTML.Window (localStorage)
 
-data DeleteUserWidgetAction = PleaseDelete | DoNothing | FailedDelete AppError | Done
+data DeleteUserWidgetAction = PleaseDelete DeleteFormValue | DoNothing | FailedDelete AppError | Done
 
 type Credentials = {username:: Maybe String, password:: Maybe String }
+
+type DeleteFormValue = {username :: String, password :: String, notRecoverable :: Boolean}
+emptyFormValues = {username: "", password: "", notRecoverable: false}
 
 emptyCredentials :: Credentials
 emptyCredentials = {username: Nothing, password: Nothing}
@@ -51,24 +54,18 @@ emptyCredentials = {username: Nothing, password: Nothing}
 deleteUserWidget :: Widget HTML Unit
 deleteUserWidget = do
   credentials :: Credentials <- liftEffect $ either (\_ -> {username: Nothing, password: Nothing}) (\s -> {username: s.username, password: s.password}) <$> getAppState
-  newIndex <- ((Right (Index Nil)) <$ (deleteForm emptyCredentials (Enabled false))) <|> (liftAff $ runExceptT $ getIndex)
+  newIndex <- ((Right (Index Nil)) <$ (deleteForm emptyCredentials emptyFormValues (Enabled false))) <|> (liftAff $ runExceptT $ getIndex)
   case newIndex of
     Right index -> go index credentials Default
-    Left err -> div' [text (show err), void (deleteForm emptyCredentials (Enabled false))] 
+    Left err -> div' [text (show err), void (deleteForm emptyCredentials emptyFormValues (Enabled false))] 
 
   where
     go :: Index -> Credentials -> WidgetState -> Widget HTML Unit
     go index@(Index entries) credentials state = do
       res <- case state of
-        Default -> deleteForm credentials (Enabled true)
+        Default -> deleteForm credentials emptyFormValues (Enabled true)
         Loading -> do
-          -- let deleteUserAndPin = do
-          --                         deleteUser index
-          --                         (ExceptT $ Right <$> (liftEffect (window >>= localStorage))) >>= (\v -> mapExceptT liftEffect (deleteCredentials v)) 
-          -- let deleteUserOp = liftAff $ (either FailedDelete (\_ -> Done) <$> (runExceptT $ deleteUserAndPin))
-          -- div' [loadingDiv, simpleButton "Delete account" true DoNothing] <|> deleteUserOp
           let total = length entries
-          -- res <- runOperation (Right unit) $ deleteUserSteps index (\i -> p [] [text $ "Deleting cards: " <> (loadingBar i total 80)]) text
           res <- runOperation (Right unit) $ deleteUserSteps index (\i -> div [Props._id "deleteUserArea"] [form [] [
             h1 [] [text "Delete account"]
           , div [Props.className "loadingBarContainer"] [
@@ -81,42 +78,26 @@ deleteUserWidget = do
             Left (Left err) -> FailedDelete err
             Right _ -> FailedDelete $ InvalidOperationError "Operation was not completed, but no other steps were programmed."
             Left (Right _) -> Done
-        Error err -> div' [text err, deleteForm credentials (Enabled true)] 
+        Error err -> div' [text err, deleteForm credentials emptyFormValues (Enabled true)] 
       case res of
-        PleaseDelete -> do
-          confirmation <- div [] [confirmationWidget "Are you sure you want to delete your account? You won't be able to recover it."]
+        PleaseDelete f -> do
+          confirmation <- (false <$ deleteForm credentials f (Enabled false)) <|> confirmationWidget "Are you sure you want to delete your account? You won't be able to recover it."
           if confirmation then go index credentials Loading else go index credentials Default
         DoNothing -> go index credentials Default
         FailedDelete err -> go index credentials (Error (show err))
         Done -> pure unit
 
-    deleteForm :: Credentials -> Enabled -> Widget HTML DeleteUserWidgetAction
-    deleteForm credentials (Enabled enabled) = div [Props._id "deleteUserArea"] [form [Props.disabled (not enabled)] [
+    deleteForm :: Credentials -> DeleteFormValue -> Enabled -> Widget HTML DeleteUserWidgetAction
+    deleteForm credentials formValues (Enabled enabled) = div [Props._id "deleteUserArea"] [form [Props.disabled (not enabled)] [
       h1 [] [text "Delete account"]
     , demand $ do
-        formValues <- loopS ({username: "", password: "", notRecoverable: false}) $ \{username, password, notRecoverable} -> do
+        formValues' <- loopS formValues $ \{username, password, notRecoverable} -> do
           username' :: String  <- loopW username $ verySimpleInputWidget (InputType "text")     (ClassName "username") (Label "Username")   (Enabled true) (Placeholder "username")   (matchingValueClassName credentials.username)
           password' :: String  <- loopW password $ verySimpleInputWidget (InputType "password") (ClassName "password") (Label "Passphrase") (Enabled true) (Placeholder "passphrase") (matchingValueClassName credentials.password)
-          -- username' :: String <- simpleUserSignal "username" username
-          -- password' :: String <- loopW password (simplePasswordInputWidget "password" (text "Password"))
           checkbox' :: Boolean <- simpleCheckboxSignal "warning" (text "All my data will be permanently deleted. I understand that this action cannot be undone or canceled.") notRecoverable
           pure { username: username', password: password', notRecoverable: checkbox' }
-        fireOnce (submitWidget formValues)
+        fireOnce (simpleButton "delete" "Delete account" (not ((Just formValues'.username) == credentials.username && (Just formValues'.password) == credentials.password && formValues'.notRecoverable)) (PleaseDelete formValues'))
     ]]
 
     matchingValueClassName :: Maybe String -> String -> Maybe ClassName
     matchingValueClassName expectedValue value = expectedValue >>= (\expectedValue' -> if expectedValue' == value then (Just $ ClassName "valid") else Nothing)
-
-    submitWidget :: { username :: String, password :: String, notRecoverable :: Boolean } -> Widget HTML DeleteUserWidgetAction
-    submitWidget f@{ username, password, notRecoverable } = do
-      check <- liftEffect $ if username == "" || password == "" then pure (Right false) else checkC f
-      disabled  <- pure $ case check of
-        Left  err -> true
-        Right b   -> not (b && notRecoverable)
-
-      simpleButton "delete" "Delete account" disabled PleaseDelete
-    
-    checkC :: { username :: String, password :: String, notRecoverable :: Boolean } -> Effect (Either AppError Boolean)
-    checkC { username, password } = runExceptT $ do
-      { username: u, password: p } <- ExceptT $ liftEffect $ getAppState
-      except $ Right $ u == (Just username) && p == (Just password)
