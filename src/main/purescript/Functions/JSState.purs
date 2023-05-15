04c134c4123d0c2010bfe811000d2386ca06abee
Fabio Solaroli <fabio@solaroli.it>
2023-05-15 02:45:21 +0200

    Add signature for updateAppState

--- src/main/purescript/Functions/JSState.purs
+++ src/main/purescript/Functions/JSState.purs
@@ -1,22 +1,33 @@
 module Functions.JSState where
 
-import Control.Bind (bind)
 import Control.Applicative (pure)
-import Control.Semigroupoid ((>>>))
+import Control.Bind (bind)
+import Control.Monad (class Monad)
 import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
+import Control.Semigroupoid ((>>>))
 import Data.Argonaut.Core (stringify)
 import Data.Argonaut.Decode.Class (decodeJson)
 import Data.Argonaut.Encode.Class (encodeJson)
 import Data.Argonaut.Parser (jsonParser)
+import Data.BigInt (BigInt)
 import Data.Either (Either(..))
 import Data.Function (($))
 import Data.Functor ((<$>))
+import Data.HexString (HexString)
+import Data.Map (Map)
+import Data.Maybe (Maybe)
 import Data.Semigroup ((<>))
 import Data.Show (show)
 import Data.Unit (Unit, unit)
-import DataModel.AppState (AppState, AppError(..), InvalidStateError(..))
+import DataModel.AppState (AppError(..), AppState, HashState, InvalidStateError(..), KDFState)
+import DataModel.AsyncValue (AsyncValue)
+import DataModel.Card (Card(..))
+import DataModel.FragmentData (FragmentData)
+import DataModel.Proxy (Proxy)
+import DataModel.User (UserCard(..), UserInfoReferences(..), UserPreferences(..))
 import Effect (Effect)
-import Effect.Class (liftEffect)
+import Effect.Class (class MonadEffect, liftEffect)
+import Prim.Row (class Nub, class Union)
 import Record (merge)
 
 foreign import getJsonState :: Unit -> Effect String
@@ -35,6 +46,59 @@ foreign import updateJsonState :: String -> Effect Unit
 modifyAppState :: AppState -> Effect Unit
 modifyAppState = encodeJson >>> stringify >>> updateJsonState
 
+updateAppState :: forall m r1 r3.
+  Monad m => MonadEffect m => Union r1
+                                      ( c :: Maybe HexString
+                                      , cardsCache :: Map HexString Card
+                                      , currentChallenge :: Maybe
+                                                              { cost :: Int
+                                                              , toll :: HexString
+                                                              }
+                                      , fragmentData :: Maybe FragmentData
+                                      , hash :: HashState
+                                      , p :: Maybe HexString
+                                      , password :: Maybe String
+                                      , proxy :: Proxy
+                                      , sessionKey :: Maybe HexString
+                                      , srpInfo :: { group :: { g :: BigInt
+                                                              , nn :: BigInt
+                                                              }
+                                                   , k :: BigInt
+                                                   , kdf :: KDFState
+                                                   }
+                                      , toll :: AsyncValue HexString
+                                      , userCard :: Maybe UserCard
+                                      , userInfoReferences :: Maybe UserInfoReferences
+                                      , userPreferences :: Maybe UserPreferences
+                                      , username :: Maybe String
+                                      )
+                                      r3
+                           => Nub   r3
+                                      ( c :: Maybe HexString
+                                      , cardsCache :: Map HexString Card
+                                      , currentChallenge :: Maybe
+                                                              { cost :: Int
+                                                              , toll :: HexString
+                                                              }
+                                      , fragmentData :: Maybe FragmentData
+                                      , hash :: HashState
+                                      , p :: Maybe HexString
+                                      , password :: Maybe String
+                                      , proxy :: Proxy
+                                      , sessionKey :: Maybe HexString
+                                      , srpInfo :: { group :: { g :: BigInt
+                                                              , nn :: BigInt
+                                                              }
+                                                    , k :: BigInt
+                                                    , kdf :: KDFState
+                                                    }
+                                      , toll :: AsyncValue HexString
+                                      , userCard :: Maybe UserCard
+                                      , userInfoReferences :: Maybe UserInfoReferences
+                                      , userPreferences :: Maybe UserPreferences
+                                      , username :: Maybe String
+                                      )
+                                      => Record r1 -> m (Either AppError Unit)
 updateAppState partialState = runExceptT $ do
   stateToUpdate <- ExceptT $ liftEffect $ getAppState
   ExceptT $ Right <$> (liftEffect $ modifyAppState (merge partialState stateToUpdate))
