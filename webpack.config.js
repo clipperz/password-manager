6dacb621ac0b418e9519d3ad38f3b1ccab7d2fd2
Fabio Solaroli <fabio@solaroli.it>
2023-08-02 18:42:23 +0200

    Change fragment position in url for share and redeem

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,8 @@ module.exports = (env) => {
 			new webpack.EnvironmentPlugin({
 				CURRENT_COMMIT: git('rev-parse HEAD'),
 				APP_URL:	"/" + (env.production ? 'app' : 'index.html'),
-				SHARE_URL:  "/" + (env.production ? 'share#' : 'share_index.html#'),
-				REDEEM_URL: "/" + (env.production ? 'share/redeem#' : 'redeem_index.html#'),
+				SHARE_URL:  "/" + (env.production ? 'share/#' : 'share_index.html#'),
+				REDEEM_URL: "/" + (env.production ? 'share/redeem/#' : 'redeem_index.html#'),
 			}),
 			// app html package configuration
 			new HtmlWebpackPlugin({
